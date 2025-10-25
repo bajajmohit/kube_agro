071ce6171218fe033c26b469a16a48f3be4bd629
bajajmohit <bajajmohit08@gmail.com>
2025-10-25 09:58:49 +0400

    Update default AWS region in variables.tf from us-west-2 to eu-central-1

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "us-west-2"
+  default     = "eu-central-1"
 }
 
 variable "project_name" {
